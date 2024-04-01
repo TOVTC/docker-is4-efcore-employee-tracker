74926ad7660f593fa73e8f6dad528457cbc238fd
Veronica Tai Chi To <99231049+TOVTC@users.noreply.github.com>
2024-04-01 11:25:07 -0400

    database queries for backend controllers

--- server/EmployeeTracker/Controllers/AboutController.cs
+++ server/EmployeeTracker/Controllers/AboutController.cs
@@ -1,5 +1,7 @@
-﻿using EmployeeTracker.Models;
+﻿using EmployeeTracker;
+using EmployeeTracker.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api.Controllers
 {
@@ -7,12 +9,23 @@ namespace Api.Controllers
     [Route("api/[controller]")]
     public class AboutController : Controller
     {
-        public AboutUs Get()
+        private readonly EmployeeTrackerDbContext _context;
+        public AboutController(EmployeeTrackerDbContext context)
         {
-            return new AboutUs
+            _context = context;
+        }
+
+        // GET: api/about
+        [HttpGet]
+        public async Task<ActionResult<AboutUs>> GetAbout()
+        {
+            if (_context.About == null)
             {
-                Description = "At our company, imagination takes form and creativity knows no bounds. We are a dynamic and innovative creative agency committed to transforming visions into vibrant, tangible realities. We are here to infuse your brand, project, or campaign with the kind of creativity that leaves a lasting impression. Our diverse team of visionaries, artists, strategists, and storytellers is united by a common goal: to craft exceptional experiences and narratives that captivate, inspire, and engage.\r\n\r\nWhether you're a startup seeking an identity or an established brand looking to refresh, we're here to turn your aspirations into a vibrant mosaic of success. Join us, and together, we'll bring your vision to life, one piece at a time. Your vision is our mission, and we're excited to explore the limitless possibilities with you."
-            };
+                return NotFound();
+            }
+
+            return await _context.About
+                .FirstOrDefaultAsync();
         }
     }
 }
